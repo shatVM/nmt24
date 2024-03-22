95d1cdf1688ddc9e02233d98dd26f31646ee49a7
Vlad Gerasimchuk <88591743+2Voby@users.noreply.github.com>
2024-03-22 17:56:18 +0200

    Delete client directory

--- client/scripts/components/dropdown.js
+++
@@ -1,9 +0,0 @@
-export function dropdownMenu(dropdown) {
-  dropdown.addEventListener("click", function () {
-    if (dropdown.classList.contains("opened")) {
-      dropdown.classList.remove("opened");
-    } else {
-      dropdown.classList.add("opened");
-    }
-  });
-}
