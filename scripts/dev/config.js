ddb6131b8b23cf513f93e9486900ff2b34606aec
shatVM <90845743+shatVM@users.noreply.github.com>
2024-04-23 17:01:32 +0300

    2

--- scripts/dev/config.js
+++ scripts/dev/config.js
@@ -1,9 +1,9 @@
-let admMode = 1;
+let admMode = 0;
 // 1 - автоматично вибирати 2 перших чекбокса
 // 0 - автоматично НЕ вибирати чекбокси
 
-let showTestFinishButton = 0;
-let status = 3;
+let showTestFinishButton = 1;
+let status = 2;
 
 // 1 - dev
 // 2 - render
