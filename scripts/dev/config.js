564817d06433843132f523c6608ce1708650e0b0
shatVM <90845743+shatVM@users.noreply.github.com>
2024-05-17 08:39:02 +0300

    Update config.js

--- scripts/dev/config.js
+++ scripts/dev/config.js
@@ -22,7 +22,7 @@ let api;
 let countOfStreams = 0;
 
 //Показувати правильні результати в профілі
-let showCorrectAnswersInProfile = 1;
+let showCorrectAnswersInProfile = 0;
 
 if (status == 1) {
   client = "http://127.0.0.1:5500/client";
