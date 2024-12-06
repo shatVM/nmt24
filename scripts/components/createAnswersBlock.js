57feab2c4157f28ab1e35e1a86e77c1917cc2140
Artemka1806 <74206792+Artemka1806@users.noreply.github.com>
2024-12-06 12:48:31 +0200

    хз чи буде працбвать

--- scripts/components/createAnswersBlock.js
+++ scripts/components/createAnswersBlock.js
@@ -215,7 +215,7 @@ export function createSubjectResultBlock(
     }
   }
 
-  let scoreBlock = subjectElement.querySelector(".result-item__score");
+  let scoreBlock = subjectElement.querySelector(".user-results__item");
   if (scoreBlock) {
     scoreBlock.addEventListener("click", function () {
       subjectElement.classList.toggle("active");
