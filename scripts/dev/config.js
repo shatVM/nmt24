10c12e7a78e0f6cd17d3cfbc169b2334f38c1941
shatVM <90845743+shatVM@users.noreply.github.com>
2025-05-02 09:46:20 +0300

    Update configuration settings and status handling

--- scripts/dev/config.js
+++ scripts/dev/config.js
@@ -5,73 +5,43 @@ let admMode = 0;
 //Показувати кнопку завершення тестування  
 let showTestFinishButton = 1;
 
-//Показувати блок запитання в профілі
-let showQuestionInProfile = 1;
-
-//Показувати відповіді учня в профілі
-let showUserAnswersInProfile = 1;
-
-//Показувати правильні результати в профілі
-let showCorrectAnswersInProfile = 0;
-
-// Кількість iframe на youtube.html
-let countOfStreams = 3;
+let status = 4;
 
 let client;
 let api;
 let ws_api;
 
-let status = 10;
+//let status = 10;
 
-switch (status) {
-  // 1 - dev 127.0.0.1
-  case 1:
-    client = "http://127.0.0.1:5500/client";
-    api = "http://localhost:5050/rest"; 
-    ws_api = "ws://localhost:5060/";
-    break;
-  // 2 - render
-  case 2:
-    client = "https://shatvm.github.io/nmt24";
-    api = "https://nmt-server-o801.onrender.com/rest";
-    ws_api = "wss:/nmt-server-o801.onrender.comt:5060/";
-    break;
-  // 3 - host 3003
-  case 3:
-    client = "http://10.14.33.5/nmt24/nmt-client";
-    api = "http://10.14.33.5:5050/rest";
-    ws_api = "ws://localhost:5060/";
-      break;
-  // 4 - host 10.15.131.218
-  case 4:
-    client = "https://10.15.131.218/nmt24/nmt-client";
-    api = "https://10.15.131.218/rest";
-    ws_api = "wss://localhost:5060/";
-    break;
-  // 5 - dev спеціально для Live Server в Sublime Text
-  case 5:
-    client = "http://localhost:5500";
-    api = "http://localhost:8080/rest";
-    ws_api = "wss://localhost:5060/";
-    break;
-  // 6 - turbo-telegram-vwxx54w6vxqhx9rq-5500.app.github.dev
-  case 6:
-    client = "https://turbo-telegram-vwxx54w6vxqhx9rq-5500.app.github.dev";
-    api = "https://nmt-server.onrender.com/rest";
-    ws_api = "wss://nmt-server.onrender.com/";
-    break;
-  // 10 - docker контейнер на серваку політеха
-  case 10:
-    client = "https://shatvm.github.io/nmt24";
-    api = "https://services.lyceum.ztu.edu.ua/nmt24/rest";
-    ws_api = "";
-    break;
-  // 11 - клієнт на локалці, сервер docker контейнер на серваку політеха
-  case 11:
-    client = "http://localhost:5500";
-    api = "https://services.lyceum.ztu.edu.ua/nmt24/rest";
-    ws_api = "";
-    break;
+let countOfStreams = 0;
+
+//Показувати правильні результати в профілі
+let showCorrectAnswersInProfile = 1;
+
+if (status == 1) {
+  client = "http://127.0.0.1:5500/client";
+  api = "http://localhost:5050/rest";
+  ws_api = "ws://localhost:5060/";
+} else if (status == 2) {
+  client = "https://shatvm.github.io/nmt24";
+  api = "https://nmt-server.onrender.com/rest";
+  ws_api = "wss://localhost:5060/";
+} else if (status == 3) {
+  client = "http://10.14.33.5/nmt24/nmt-client";
+  api = "http://10.14.33.5:5050/rest";
+  ws_api = "ws://localhost:5060/";
+} else if (status == 4) {
+  client = "http://10.15.131.218/nmt24/nmt-client";
+  api = "http://10.15.131.218/rest";
+  ws_api = "wss://localhost:5060/";
+} else if (status == 5) {
+  client = "http://localhost:5500";
+  api = "http://localhost:5050/rest";
+  ws_api = "wss://localhost:5060/";
+} else if (status == 6) {
+  client = "https://turbo-telegram-vwxx54w6vxqhx9rq-5500.app.github.dev";
+  api = "https://nmt-server.onrender.com/rest";
+  ws_api = "wss://nmt-server.onrender.com/";
 }
 
 
